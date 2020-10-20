718766088c18c34eb862a3104ebc8d4f03aee007
Mr John <38257989+MrJohnDev@users.noreply.github.com>
2020-10-20 17:42:17 +0700

    Add files via upload

--- flask_ngrok_url_tg.py
+++ flask_ngrok_url_tg.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from pyngrok import ngrok
 import threading,time
-
+import telebot
 
 app = Flask(__name__)
 port = 4567
+bot_token = "1234567890:AABBCCDDEEffggaasadasb_hfgm6eJ5df"
+telegram_user_id_for_new_url=123456789
+
+bot = telebot.TeleBot(bot_token)
 
 @app.route('/', methods=['post', 'get'])
 def main():
@@ -15,7 +19,8 @@ def ngrok_run():
 		public_url = ngrok.connect(port,return_ngrok_tunnel=True)
 
 		print("\n\t----------------------------\n\n\t"+public_url.public_url+"\n\n\t----------------------------\n")
-
+		print('\t✅ ngrok Working')
+		bot.send_message(telegram_user_id_for_new_url,str(public_url.public_url))
 		save_time = int(time.time())
 		while True:
 			time.sleep(60*10)
@@ -25,10 +30,22 @@ def ngrok_run():
 		try:
 			ngrok.disconnect(ngrok.get_tunnels()[0].public_url)
 			ngrok.kill()
-			print("\tRestarting server\n\n\n\n")
+			print("\t🔄 Restarting server\n\n\n\n")
 		except Exception:
 			pass
 
+def bot_run():
+	while True:
+		try:
+			print('\t✅ Bot Working')
+			bot.polling()
+			sys.exit()
+		except Exception as e:
+			print(e)
+			print('\t🔄 Restart')
+
 if __name__ == '__main__':
+	threading.Thread(target=bot_run).start()
+	time.sleep(5)
 	threading.Thread(target=ngrok_run).start()
 	app.run(host='0.0.0.0', port=port)
